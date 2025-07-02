521168f49b277491ee7a823fbd7496e20c3e52cb
NL <138875891+likhithan2001@users.noreply.github.com>
2025-07-02 12:37:17 +0530

    Update assetRoutes.js

--- backend/routes/assetRoutes.js
+++ backend/routes/assetRoutes.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const assetController = require('../controllers/assetController');
-const { verifyToken } = require('../middleware/auth');
 
-router.post('/', verifyToken, assetController.createAsset);
-router.post('/bulk', verifyToken, assetController.createMultipleAssets);
-router.get('/', verifyToken, assetController.getAssets);
-router.put('/:id', verifyToken, assetController.updateAsset);
-router.delete('/:id', verifyToken, assetController.deleteAsset);
-
-module.exports = router;
